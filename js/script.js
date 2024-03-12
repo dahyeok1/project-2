23aff2bb4b8645b89aa30bbdec0e827a15f694ae
dahyeok1 <dahyeok1@uci.edu>
2024-03-12 00:07:58 -0700

    n

--- js/script.js
+++ js/script.js
@@ -1,58 +1,50 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
 $(document).ready(function() {
-
-    $("#cat-img").hide();
-    $("#happy-dog").hide();
-    $("#secret-text").hide();
+    $("#rs").hide();
+        $("#triangle").click(function(){
+            $("#rs").show();
+        });
+        $("#rs").click(function(){
+            $("#rs").hide();
+        });
     
+    $("#happy").hide();
+        $("#sad").click(function(){
+            $("#happy").show();
+            $("#sad").hide();
+        });
+        $("#happy").click(function(){
+            $("#sad").show();
+            $("#happy").hide();
+        });
 
+    $("#recycles").hide();
+        $("#bottles").click(function(){
+            $("#recycles").show();
+            $("#bottles").hide();
+        });
+        $("#recycles").click(function(){
+            $("#bottles").show();
+            $("#recycles").hide();
+        });
+    
+    $("#what").click(function(){
+        $(".three").addClass("animate-img");
 
-    $("#doggo-img").click(function(){
-        $("#doggo-img").hide();
-        $("#cat-img").show();
-
-    });
-
-    $("#cat-img").click(function(){
-        // $("#doggo-img").show();
-        $("#cat-img").hide();
-        $("#happy-dog").show();
-
-    });
-
-    $("#happy-dog").click(function(){
-        $("#doggo-img").show();
-        $("#cat-img").hide();
-        $("#happy-dog").hide();
-
-    });
-
-    $("#toggle-text").click(function(){
-        $("#secret-text").toggle();
-
-    });
-
-    $("#change-bg-color").click(function(){
-        $("#home-screen").css("background-color","khaki");
-
-        // change the font color of the text.
-        $("#change-bg-color").css("color","white");
-
-    });
-
-    $("#secret-text").click(function(){
-        $(".main-img").addClass("animate-img");
-        
         setTimeout(function(){
-            $(".main-img").removeClass("animate-img");
+            $(".three").removeClass("animate-img");
         },2000);
+    })
 
+    $("#what").click(function(){
+        $(".three").addClass("animate-img");
+        setTimeout(function(){
+            $(".three").removeClass
+            ("animate-img");
+        }, 2000);
     });
-    
-
-
-
-
-
+    $("#what").css({
+        animation: "expandText 2s forwards"
+});
 });
